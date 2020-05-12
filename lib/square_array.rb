20ffaabd72867c079cee1b2acfff114bd655a3d7
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-12 09:14:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  square_array = []
+  counter = 0
+  while array[counter] do
+    puts square_array.push(array[counter**counter])
+    counter+=1
+  end
 end
